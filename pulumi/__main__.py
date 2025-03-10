e31e5036bc87e62c977d94a9a98b92100c198b9a
Jon Frankel <jonathan@frankel.dev>
2025-03-09 21:56:28 -0500

    Add files to the bucket and make it a website

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,10 +1,62 @@
-"""An AWS Python Pulumi program"""
+import mimetypes
+import os
 
 import pulumi
 from pulumi_aws import s3
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+bucket = s3.BucketV2("resume-bucket")
+bucket_name = bucket.id
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+website = s3.BucketWebsiteConfigurationV2(
+    "resume-website",
+    bucket=bucket_name,
+    index_document={
+        "suffix": "index.html",
+    },
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    "resume-public-access-block",
+    bucket=bucket_name,
+    block_public_acls=False,
+)
+
+files = os.listdir("./app/public")
+bucketObjects = []
+for file in files:
+    mime_type, _ = mimetypes.guess_type(file)
+    bucketObjects.append(
+        s3.BucketObject(
+            file,
+            bucket=bucket_name,
+            source=pulumi.FileAsset(f"./app/public/{file}"),
+            acl="public-read",
+            content_type=mime_type,
+        )
+    )
+
+public_read_policy = pulumi.Output.json_dumps(
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": ["s3:GetObject"],
+                "Resource": [
+                    pulumi.Output.format("arn:aws:s3:::{0}/*", bucket_name),
+                ],
+            }
+        ],
+    }
+)
+
+bucket_policy = s3.BucketPolicy(
+    "bucket-policy",
+    bucket=bucket_name,
+    policy=public_read_policy,
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block]),
+)
+
+pulumi.export("bucket_name", bucket_name)
+pulumi.export("website_url", website.website_endpoint)
