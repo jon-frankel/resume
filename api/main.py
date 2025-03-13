b84c66c2c64a535c6e665612a3a1c118e31effb1
Jon Frankel <jonathan@frankel.dev>
2025-03-12 22:28:34 -0500

    Fix that

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
-def handler(event=None, context=None):
+def handler(event=None, _context=None):
     return {
         "statusCode": 200,
         "content-type":"application/json",
         "body": {
             "message": "Hello, World!",
             "event": event,
-            "context": context
         }
     }
