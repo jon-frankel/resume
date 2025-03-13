9a215f4c3cdf8a893cf96d02a59e0e7ebac3743d
Jon Frankel <jonathan@frankel.dev>
2025-03-13 00:58:18 -0500

    Oops

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -61,8 +61,9 @@ bucket_policy = aws.s3.BucketPolicy(
     opts=pulumi.ResourceOptions(depends_on=[public_access_block]),
 )
 
-# Set up permissions for lambda
 lambda_name = "web-counter-lambda"
+
+# Lambda role
 assume_role = aws.iam.get_policy_document(
     statements=[
         {
@@ -78,7 +79,7 @@ lambda_role = aws.iam.Role(
     assume_role_policy=assume_role.json,
 )
 
-# Give the lambda function permission to write logs
+# Lambda logging permissions
 log_group = aws.cloudwatch.LogGroup(
     f"{lambda_name}-log-group",
     name=f"/aws/lambda/{lambda_name}",
@@ -104,36 +105,6 @@ log_policy_attachment = aws.iam.RolePolicyAttachment(
     f"{lambda_name}-log-policy-attachment",
     role=lambda_role.name,
     policy_arn=log_policy.arn,
-)
-
-# Now create the lambda
-lambda_archive = archive.get_file(
-    type="zip",
-    source_file="../api/main.py",
-    output_path="api.zip",
-)
-lambda_function = aws.lambda_.Function(
-    lambda_name,
-    code=pulumi.FileArchive("api.zip"),
-    name=lambda_name,
-    role=lambda_role.arn,
-    handler="main.handler",
-    source_code_hash=lambda_archive.output_base64sha256,
-    runtime=aws.lambda_.Runtime.PYTHON3D13,
-    environment={"variables": {"BUCKET_NAME": bucket_name}},
-    logging_config={"log_format": "Text"},
-    opts=pulumi.ResourceOptions(depends_on=[log_policy_attachment, log_group]),
-)
-
-# And make it accessible via HTTP
-lambda_url = aws.lambda_.FunctionUrl(
-    f"{lambda_name}-url",
-    function_name=lambda_function.name,
-    authorization_type="NONE",
-    cors={
-        "allow_origins": ["*"],
-        "allow_methods": ["*"],
-    }
 )
 
 # DynamoDB table for visit counter
@@ -188,8 +159,38 @@ dynamodb_policy_attachment = aws.iam.RolePolicyAttachment(
     policy_arn=dynamodb_policy.arn,
 )
 
-# Add table name to lambda environment
-lambda_function.environment["variables"]["TABLE_NAME"] = visits_table.name
+# Now create the lambda
+lambda_archive = archive.get_file(
+    type="zip",
+    source_file="../api/main.py",
+    output_path="api.zip",
+)
+lambda_function = aws.lambda_.Function(
+    lambda_name,
+    code=pulumi.FileArchive("api.zip"),
+    name=lambda_name,
+    role=lambda_role.arn,
+    handler="main.handler",
+    source_code_hash=lambda_archive.output_base64sha256,
+    runtime=aws.lambda_.Runtime.PYTHON3D13,
+    environment={"variables": {
+        "BUCKET_NAME": bucket_name,
+        "TABLE_NAME": visits_table.name,
+    }},
+    logging_config={"log_format": "Text"},
+    opts=pulumi.ResourceOptions(depends_on=[log_policy_attachment, log_group]),
+)
+
+# And make it accessible via HTTP
+lambda_url = aws.lambda_.FunctionUrl(
+    f"{lambda_name}-url",
+    function_name=lambda_function.name,
+    authorization_type="NONE",
+    cors={
+        "allow_origins": ["*"],
+        "allow_methods": ["*"],
+    }
+)
 
 pulumi.export("bucket_name", bucket_name)
 pulumi.export("bucket_bucket", bucket.bucket)
