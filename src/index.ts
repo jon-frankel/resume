6e7fd1eede76e3f0006393cbfc087c715bb21863
Jon Frankel <jon-frankel@users.noreply.github.com>
2025-03-17 21:07:33 -0500

    A small change to trigger a deployment

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ type ResponseBody = {
 
 export const handler = async (): Promise<ResponseBody> => {
     const response = await fetch(LAMBDA_URL);
-    console.log('Response:', response);
-    return await response.json();
+    return await response?.json();
 };
 
 // On load, fetch the number of visits from the server and display it
@@ -17,6 +16,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         const data = await handler();
         document.getElementById('visits').innerText = data.visits.toString();
     } catch (error) {
-        console.log('Error fetching data:', error);
+        console.error('Error fetching data', error);
     }
 });
