8357ba2b7e739a1e7a98edcccf8340ebad631678
Jon Frankel <jon-frankel@users.noreply.github.com>
2026-02-16 16:07:11 -0600

    merge

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-const host = process.env.URL ?? 'http://localhost';
+// Localstack does not actually return the URL for the s3 site, but this it.
+const localstackUrl = 'http://resume-local.frankel.test.s3-website.localhost.localstack.cloud:4566';
+
+const host = process.env.URL ?? localstackUrl;
 console.log("Testing host: ", host);
 
 test('page contains name', async ({ page }) => {
@@ -13,7 +16,7 @@ test('page contains name', async ({ page }) => {
 test('web counter exists', async ({ page }) => {
   await page.goto(host);
 
-  // wait for network
+  // wait for network idle to ensure the counter has been updated
   await page.waitForLoadState('networkidle');
 
   const counter = page.getByText(/Web counter: \d+ site visits/);
